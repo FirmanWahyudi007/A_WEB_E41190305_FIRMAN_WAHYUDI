512be4c91735180fab17e37920901e41400aa697
Firman Wahyudi <e41190305@student.polije.ac.id>
2021-03-24 09:40:45 +0700

    bkpm 3/2 sampai 3/4

--- belajarlaravel/routes/web.php
+++ belajarlaravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ManagementUserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+  return view('home');
 });
+Route::get('user',[ManagementUserController::class, 'index'])->name('user.index');
+Route::get('user/create',[ManagementUserController::class, 'create'])->name('user.create');
+Route::post('user',[ManagementUserController::class, 'store'])->name('user.store');
+Route::get('user/{user}',[ManagementUserController::class, 'show'])->name('user.show');
+Route::put('user/{user}',[ManagementUserController::class, 'update'])->name('user.update');
+Route::delete('user/{user}',[ManagementUserController::class, 'delete'])->name('user.delete');
+Route::get('user/{user}/edit',[ManagementUserController::class, 'edit'])->name('user.edit');
